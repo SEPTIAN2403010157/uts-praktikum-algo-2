4175356b6d33d95f8c74f8e1519fbd424450713a
SEPTIAN2403010157 <2403010157@unper.ac.id>
2024-11-14 23:06:12 +0700

    Create soal_4.py

---
+++ soal_4.py
@@ -0,0 +1,8 @@
+kalimat = input("Masukkan kalimat: ")
+vokal = "aeiouAEIOU"
+jumlah_konsonan = 0
+for char in kalimat:
+    
+    if char.isalpha() and char not in vokal:
+        jumlah_konsonan += 1
+print(f"Jumlah huruf konsonan: {jumlah_konsonan}")
