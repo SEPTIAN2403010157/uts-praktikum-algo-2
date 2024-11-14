59f5aa46effc85df2712c8438a39d9368feb9ff2
SEPTIAN2403010157 <2403010157@unper.ac.id>
2024-11-14 23:05:00 +0700

    Create soal_3.py

---
+++ soal_3.py
@@ -0,0 +1,5 @@
+kata_pertama = input("Masukkan kata pertama: ")
+kata_kedua = input("Masukkan kata kedua: ")
+kata_ketiga = input("Masukkan kata ketiga: ")
+hasil_gabungan = kata_pertama + " " + kata_kedua + " " + kata_ketiga
+print("Hasil penggabungan:", hasil_gabungan)
